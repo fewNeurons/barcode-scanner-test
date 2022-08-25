e899fcc7865921c130e0da4ce8d68ce0e11a7ac3
Dani <daniel@berebel.io>
2022-08-25 14:14:26 +0200

    ml-kit checkpermissions does not return

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+// import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -10,16 +10,17 @@ export class Tab1Page {
   constructor() {}
   async startScan() {
     // BarcodeScanner.hideBackground(); // make background of WebView transparent
-    console.log(BarcodeScanner);
+    console.log('tryin to scan!');
+    // console.log(BarcodeScanner);
     const status = await BarcodeScanner.checkPermissions();
     console.log('status:', status);
     if (status !== 'granted') {
-      console.log(
-        'some problem with the camera\n',
-        'If you want to grant permission for using your camera, enable it in the app settings.'
-      );
-      const req = await BarcodeScanner.requestPermissions();
-      console.log('request:', req);
+      // console.log(
+      //   'some problem with the camera\n',
+      //   'If you want to grant permission for using your camera, enable it in the app settings.'
+      // );
+      // const req = await BarcodeScanner.requestPermissions();
+      // console.log('request:', req);
     } else if (status === 'granted') {
       console.log('camera permission granted!');
     }
@@ -27,8 +28,9 @@ export class Tab1Page {
     const result = await BarcodeScanner.start(); // start scanning and wait for a result
 
     // if the result has content
-    if (result) {
-      console.log(result); // log the raw scanned content
-    }
+    console.log('result?');
+    console.log(result); // log the raw scanned content
+    // if (result) {
+    // }
   }
 }
