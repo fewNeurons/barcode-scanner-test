6a7cc22412ad2e81b9a93e44cfa074584a006159
fewNeurons <ferranramirezmarti@gmail.com>
2022-08-26 12:46:41 +0200

    made it work with capacitor camera plugin

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,67 +1,73 @@
-import { Component } from '@angular/core';
-// import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { Component, OnInit } from '@angular/core';
+import {
+  BarcodeFormat,
+  BarcodeScanner,
+  CameraDirection,
+  ScanResult,
+} from '@capacitor-community/barcode-scanner';
+import { Camera } from '@capacitor/camera';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  result: ScanResult;
+  scanning = false;
   constructor() {}
-  async startScan() {
-    // BarcodeScanner.hideBackground(); // make background of WebView transparent
-    console.log('tryin to scan!');
-    // console.log(BarcodeScanner);
-    const status = await BarcodeScanner.checkPermissions();
-    console.log('status:', status);
-    if (status !== 'granted') {
-      // console.log(
-      //   'some problem with the camera\n',
-      //   'If you want to grant permission for using your camera, enable it in the app settings.'
-      // );
-      // const req = await BarcodeScanner.requestPermissions();
-      // console.log('request:', req);
-    } else if (status === 'granted') {
-      console.log('camera permission granted!');
+  ngOnInit() {
+    console.log('checkin for perms');
+    if (!this.checkPermission()) {
+      console.log('no permissions granted');
+      return;
     }
-
-    const result = await BarcodeScanner.start(); // start scanning and wait for a result
-
-    // if the result has content
-    console.log('result?');
-    console.log(result); // log the raw scanned content
-    // if (result) {
-    // }
+    console.log('granted!');
+    console.log(navigator.mediaDevices);
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: 'environment' }, audio: false })
+        .then((stream) => {
+          const vidElement = document.querySelector('video');
+          vidElement.srcObject = stream;
+          vidElement.setAttribute('autoplay', '');
+          vidElement.setAttribute('muted', '');
+          vidElement.setAttribute('playsinline', '');
+          //video.src = window.URL.createObjectURL(stream);
+          vidElement.play();
+        });
+    }
   }
-  // main branch
-  // async startScan() {
-  //   BarcodeScanner.hideBackground(); // make background of WebView transparent
-  //   console.log('tryin to scan!');
-  //   // console.log(BarcodeScanner);
-  //   let status = await BarcodeScanner.checkPermission();
-  //   console.log('status:', status);
-  //   if (!status.granted) {
-  //     // console.log(
-  //     //   'some problem with the camera\n',
-  //     //   'If you want to grant permission for using your camera, enable it in the app settings.'
-  //     // );
-  //     // const req = await BarcodeScanner.requestPermissions();
-  //     status = await BarcodeScanner.checkPermission({ force: true });
-  //     if (!status.granted) {
-  //       console.error('no quiere');
-  //       const oar = await BarcodeScanner.openAppSettings();
-  //       console.log('request:', oar);
-  //     }
-  //   } else {
-  //     console.log('camera permission granted!');
-  //   }
 
-  //   const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+  async startScan() {
+    console.log(' scan starting!');
+    const options = {
+      cameraDirection: CameraDirection.BACK,
+      format: BarcodeFormat.ALL,
+    };
+    const callback = (res, err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.result = res;
+      console.log(this.result);
+    };
+    BarcodeScanner.start(options, callback);
+    this.scanning = true;
+  }
 
-  //   // if the result has content
-  //   console.log('result?');
-  //   console.log(result); // log the raw scanned content
-  //   // if (result) {
-  //   // }
-  // }
+  async stopScan() {
+    BarcodeScanner.stop();
+  }
+  async checkPermission() {
+    let status = await Camera.checkPermissions();
+    // console.log(status);
+    if (status.camera !== 'granted') {
+      Camera.requestPermissions({ permissions: ['camera'] });
+      status = await Camera.checkPermissions();
+    }
+    return status.camera === 'granted';
+  }
 }
