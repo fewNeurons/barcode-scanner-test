5f6f9133f98aaac78417aff958b94a725b3e9025
Dani <daniel@berebel.io>
2022-08-26 08:40:36 +0200

    camera installed

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -33,4 +33,35 @@ export class Tab1Page {
     // if (result) {
     // }
   }
+  // main branch
+  // async startScan() {
+  //   BarcodeScanner.hideBackground(); // make background of WebView transparent
+  //   console.log('tryin to scan!');
+  //   // console.log(BarcodeScanner);
+  //   let status = await BarcodeScanner.checkPermission();
+  //   console.log('status:', status);
+  //   if (!status.granted) {
+  //     // console.log(
+  //     //   'some problem with the camera\n',
+  //     //   'If you want to grant permission for using your camera, enable it in the app settings.'
+  //     // );
+  //     // const req = await BarcodeScanner.requestPermissions();
+  //     status = await BarcodeScanner.checkPermission({ force: true });
+  //     if (!status.granted) {
+  //       console.error('no quiere');
+  //       const oar = await BarcodeScanner.openAppSettings();
+  //       console.log('request:', oar);
+  //     }
+  //   } else {
+  //     console.log('camera permission granted!');
+  //   }
+
+  //   const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+
+  //   // if the result has content
+  //   console.log('result?');
+  //   console.log(result); // log the raw scanned content
+  //   // if (result) {
+  //   // }
+  // }
 }
